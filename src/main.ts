9750b25647ffcf2b42636096501e8ff4f537ede8
jake <jake@mealyon.com>
2026-01-15 13:40:24 +0900

    feat. sass import

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,6 @@
-import { createApp, ref } from "vue";
-import { SocketManager } from "./SocketManager";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./styles/main.scss";
 
-const app = createApp({
-  setup() {
-    const isConnected = ref(false);
-    const logs = ref<string[]>([]);
-
-    // 소켓 매니저 초기화 (로그를 화면에 찍기 위해 콜백 함수 전달)
-    const socketManager = new SocketManager("http://localhost:3005", (msg) => {
-      const time = new Date().toLocaleTimeString();
-      logs.value.unshift(`[${time}] ${msg}`);
-    });
-
-    // 연결 상태 감지
-    socketManager.onStatusChange = (status) => {
-      isConnected.value = status;
-    };
-
-    const sendOrder = () => {
-      socketManager.sendOrder({ menu: "Ice Americano" });
-    };
-
-    const clearLogs = () => (logs.value = []);
-
-    return { isConnected, logs, sendOrder, clearLogs };
-  },
-});
-
+const app = createApp(App);
 app.mount("#app");
