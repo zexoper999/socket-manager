56baebf0685bdc7d6aaa7d46ad76ee695af9a9b9
jake <jake@mealyon.com>
2026-01-15 13:34:22 +0900

    feat. vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      vue: "vue/dist/vue.esm-bundler.js",
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
